aa029bfd6916a8bdbdf614773baa60b457347c1a
Branden <br66@njit,edu>
2015-01-24 02:13:53 -0500

    player controls updated

---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float speed = 1.0f;
+	private string left = "Horizontal";
+
+	//public Animator anim;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetAxis (left) < 0)
+		{
+			Vector3 newScale = transform.localScale;
+			newScale.x = -1.0f;
+			transform.localScale = newScale;
+		}
+		else if (Input.GetAxis (left) > 0)
+		{
+			Vector3 newScale = transform.localScale;
+			newScale.x = 1.0f;
+			transform.localScale = newScale;
+		}
+	
+		transform.position += transform.right * Input.GetAxis(left) * speed * Time.deltaTime;
+		//rigidbody2D.AddForce (transform.right);
+
+	}
+}
